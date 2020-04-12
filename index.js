1eddd1858d733a51ae905cd5f910732819a50ee4
Luis Cazzullo <lcazzullo@gmail.com>
2020-04-12 16:51:22 -0300

    fixed was removed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 connectDB();
 app.use(express.json({ extended: true }))
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'))
