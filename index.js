4b95688fb9f987c100aab1778a86f57422b7ce7e
Luis Cazzullo <lcazzullo@gmail.com>
2020-04-12 15:48:37 -0300

    port was changed

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json({ extended: true }))
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'))
 app.use('/api/tasks', require('./routes/task'));
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor corre en el puerto ${PORT}`);
 })
